43df6f66763673d9b2621b570ae16dc74d43a444
ER <colorfulsing@gmail.com>
2021-03-15 17:13:10 -0600

    Track time

--- parsers/test.rb
+++ parsers/test.rb
@@ -5,6 +5,7 @@ save_outputs [{
   _collection: 'something',
   value: '123abc'  
 }]
+puts DateTime.now
 #exit 0
-raise StandardError.new('this is just a forced error to test the dup outputs fix')
+#raise StandardError.new('this is just a forced error to test the dup outputs fix')
 
