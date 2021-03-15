c2fe6c41c74eecbf68ad3cabfa09eaf6cf701d73
ER <colorfulsing@gmail.com>
2021-03-15 16:54:50 -0600

    Fix

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,7 +1,10 @@
 
 (1..5).each do |i|
+#puts i
   pages << {
-    url: 'https://fetchtest.datahen.com?a=#{i}',
+    url: "https://fetchtest.datahen.com?a=#{i}",
     page_type: 'test'
   }
 end
+
+#puts JSON.pretty_generate(pages)
