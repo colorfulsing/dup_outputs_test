dcbf56bc91605d5b24715d249b048ac643f6474d
ER <colorfulsing@gmail.com>
2021-03-15 13:53:26 -0600

    Test parsing dequeue failed

--- parsers/test.rb
+++ parsers/test.rb
@@ -2,5 +2,6 @@ save_outputs [{
   _collection: 'something',
   value: '123abc'  
 }]
-raise StandardError.new('this is just a forced error to test the dup outputs fix')
+exit 0
+#raise StandardError.new('this is just a forced error to test the dup outputs fix')
 
