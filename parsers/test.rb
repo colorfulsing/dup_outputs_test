302facd5b649f7deeb663276e37c6e9ce43ce324
ER <colorfulsing@gmail.com>
2021-03-15 15:55:37 -0600

    Test failed parse again

--- parsers/test.rb
+++ parsers/test.rb
@@ -2,6 +2,6 @@ save_outputs [{
   _collection: 'something',
   value: '123abc'  
 }]
-exit 0
-#raise StandardError.new('this is just a forced error to test the dup outputs fix')
+#exit 0
+raise StandardError.new('this is just a forced error to test the dup outputs fix')
 
